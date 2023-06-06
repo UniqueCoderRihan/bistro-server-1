660d9a5640b8c73f393c3c3eeb92cc781eb97a5f
Rihanul Islam <rihanthedev@gmail.com>
2023-06-06 16:33:14 +0600

    Payment Intent

--- index.js
+++ index.js
@@ -6,21 +6,24 @@ require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
+// Payment Secret key Setup;
+const stripe = require("stripe")(process.env.PAYMENT_STRIPE_KEY)
+
 // midileWare 
 app.use(cors());
 app.use(express.json())
 
 // Jwt Verify
-const verifyJWT = (req , res, next)=>{
+const verifyJWT = (req, res, next) => {
     const authorization = req.headers.authorization;
-    if(!authorization){
-        return res.status(401).send({error:true, message: 'UnAuthorized Access'})
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'UnAuthorized Access' })
     }
     // bearer token
     const token = authorization.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
-        if(err){
-            return res.status(401).send({error: true, message: 'UnAuthorized Access'});
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'UnAuthorized Access' });
         }
         req.decoded = decoded;
         next()
@@ -49,12 +52,12 @@ async function run() {
         await client.connect();
 
         // warning:use VerifyJWT before using verifyAdmin
-        const verifyAdmin = async(req,res,next)=>{
-            const email= req.decoded.email;
-            const query = {email: email};
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
             const user = await usersCollection.findOne(query);
-            if(user?.role !=='admin'){
-                return res.status(403).send({error:true, message:'forbidden messgae'})
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden messgae' })
             }
             next();
         }
@@ -66,14 +69,14 @@ async function run() {
         // jwt token create STEP_2 As a My Note;
         app.post('/jwt', (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn:'1h'});
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
             res.send(token);
         })
 
         // *User Management Operations
         // */ 
         // User Get APis;
-        app.get('/users', verifyJWT,verifyAdmin, async (req, res) => {
+        app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         })
@@ -94,16 +97,16 @@ async function run() {
         // check admin mail
         app.get('/users/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
-      
+
             if (req.decoded.email !== email) {
-              res.send({ admin: false })
+                res.send({ admin: false })
             }
-      
+
             const query = { email: email }
             const user = await usersCollection.findOne(query);
             const result = { admin: user?.role === 'admin' }
             res.send(result);
-          })
+        })
         // Make admin... PATCH
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
@@ -125,15 +128,15 @@ async function run() {
             res.send(result)
         })
         // Delete Item
-        app.delete('/menu/:id',verifyJWT,verifyAdmin, async(req,res)=>{
+        app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
             const id = req.params.id;
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await menuCollection.deleteOne(query);
             res.send(result);
         })
 
         // Add items for showing Menu.
-        app.post('/menu',verifyJWT,verifyAdmin, async (req,res)=>{
+        app.post('/menu', verifyJWT, verifyAdmin, async (req, res) => {
             const newItem = req.body;
             const result = await menuCollection.insertOne(newItem);
             res.send(result);
@@ -145,13 +148,13 @@ async function run() {
             const email = req.query.email;
             // console.log(email);
 
-            if(!email){
+            if (!email) {
                 res.send([]);
             }
             // check Verification.>> That's Interesting.
             const decodedEmail = req.decoded.email;
-            if(email !== decodedEmail){
-                return res.status(403).send({error:true, message: 'Porbidden  Access'})
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: true, message: 'Porbidden  Access' })
             }
 
             const query = { email: email };
@@ -160,7 +163,7 @@ async function run() {
             res.send(result)
         })
 
-        
+
         // Specific Carts Remove using Id
         app.delete('/carts/:id', async (req, res) => {
             const id = req.params.id;
@@ -168,6 +171,24 @@ async function run() {
             const result = await cartCollection.deleteOne(query);
             res.send(result)
         })
+
+        // Payment 
+        // Calculete Amount 
+        const calculteAmount = items => {
+            return items*100;
+        }
+
+        app.post('/create-payment-intent',async (req,res)=>{
+            const {price} = req.body;
+            const amount = price*100;
+            const paymentIntent =await stripe.paymentIntens.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ["card"]
+            });
+            res.send({clientSecret: paymentIntent.client_secret})
+        })
+
 
         // Add Carts On Database
         app.post('/carts', async (req, res) => {
